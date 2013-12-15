0e2995e9c2ba0aa296d5734c574b97065c1ff33b
Scott Heckel <scott.heckel@gmail.com>
2013-12-14 22:06:30 -0600

    Adding some keycodes

--- gaim.js
+++ gaim.js
@@ -174,6 +174,13 @@ window.gaim = function(gameWrapper) {
 		r: run,
 		run: run,
 		s: stop,
-		stop: stop
+		stop: stop,
+                KEYS: {
+                        LEFT: 37,
+                        UP: 38
+                        RIGHT: 39,
+                        DOWN: 40
+                }
+
 	};
 };
